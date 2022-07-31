c23db2aee6f38a8de3f4c97c0820d50e450c233e
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-07-30 21:11:17 -0500

    commit tree dc0a04d4fdebcf2047998db9bf31bdd70bf0edb4

--- src/components/AdminNavBar.js
+++ src/components/AdminNavBar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import QuestionForm from "./QuestionForm";
 
 function AdminNavBar({ onChangePage }) {
+  
+  function handleClickAndDisplayForm() {
+    onChangePage("Form")
+    return <QuestionForm />
+  }
+
   return (
     <nav>
-      <button onClick={() => onChangePage("Form")}>New Question</button>
+      <button onClick={handleClickAndDisplayForm}>New Question</button>
       <button onClick={() => onChangePage("List")}>View Questions</button>
     </nav>
   );
